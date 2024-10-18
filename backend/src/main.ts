b89195a0af2b5cee9f88f5ffadb7ad8e784bb509
Reyes-Rivera <143473424+Reyes-Rivera@users.noreply.github.com>
2024-10-18 05:43:12 -0600

    manejo de cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:5173","https://cayro.netlify.app"],
+    origin: ["https://cayro.netlify.app"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL:"http://localhost:5000",
-    // baseURL:"https://cayro-proyecto-production.up.railway.app",
+    // baseURL:"http://localhost:5000",
+    baseURL:"https://cayro-proyecto-production.up.railway.app",
     // baseURL:"https://cayro-backend.vercel.app",
     withCredentials: true,
 });
